629f200c2846ed250d36750fbc8bad967a66fb06
ricardoamdl <ricardoamdl3@gmail.com>
2026-01-20 12:28:30 -0300

    criado adição

---
+++ soma.py
@@ -0,0 +1,10 @@
+numero1 = input("Digite um número: ")
+numero2 = input("Digite outro número: ")
+
+numero1 = float(numero1)
+numero2 = float(numero2)
+
+soma = numero1 + numero2
+
+print("A soma dos números é: ", soma)
+
