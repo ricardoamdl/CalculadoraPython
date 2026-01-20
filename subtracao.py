3c7b1822d9e8bb1402f1727ec350901dc68ff7af
ricardoamdl <ricardoamdl3@gmail.com>
2026-01-20 12:40:08 -0300

    adicionado subtracao

---
+++ subtracao.py
@@ -0,0 +1,9 @@
+numero1 = input("Digite um número: ")
+numero2 = input("Digite outro número: ")
+
+numero1 = float(numero1)
+numero2 = float(numero2)
+
+subtracao = numero1 - numero2
+
+print("O resultado da subtração dos números é: ", subtracao)
