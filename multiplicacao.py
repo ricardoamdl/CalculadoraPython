92a969bf319d441df120999c8c05e634d97cdb23
ricardoamdl <ricardoamdl3@gmail.com>
2026-01-20 12:52:31 -0300

    Adicionado multiplicação

---
+++ multiplicacao.py
@@ -0,0 +1,9 @@
+numero7 = input("Digite um número: ")
+numero8 = input("Digite outro número: ")
+
+numero7 = float(numero7)
+numero8 = float(numero8)
+
+multiplicacao = numero7 * numero8
+
+print("O resultado da multiplicação é: ", multiplicacao)
